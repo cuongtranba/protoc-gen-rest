6b41bf55131c05a8f6df834d9496e020961ca0e8
cuong <bacuongtr@gmail.com>
2021-03-24 16:25:10 +0700

    add unittest

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"os"
+
 	pgs "github.com/lyft/protoc-gen-star"
 	pgsgo "github.com/lyft/protoc-gen-star/lang/go"
 )
 
 func main() {
+	f, _ := os.Open("./input.txt")
 	pgs.Init(
-		pgs.DebugEnv("DEBUG"),
+		pgs.ProtocInput(f),
 	).RegisterModule(
 		ASTPrinter(),
 		JSONify(),
