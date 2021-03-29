9e4917c8f4085ce552f8d44301ad9b581ba77c24
cuong <bacuongtr@gmail.com>
2021-03-29 15:48:37 +0700

    init router

--- plugin/gopl/go.tmpl.go
+++ plugin/gopl/go.tmpl.go
@@ -1,41 +1,5 @@
 package gopl
 
-import (
-	"net/http"
-
-	"github.com/labstack/echo/v4"
-)
-
-type UserRequest struct {
-}
-
-type UserResponse struct {
-	UserName string `json:"UserName"`
-}
-
-type UserHandler interface {
-	GetUserName(c echo.Context, request UserRequest) (err error, response UserResponse)
-}
-
-func InitUserHandlerRouter(e *echo.Echo, userHandler UserHandler) {
-	e.POST("/abc", func(c echo.Context) error {
-		var model UserRequest
-		err := c.Bind(&model)
-		if err != nil {
-			return c.JSON(http.StatusBadRequest, map[string]interface{}{
-				"error": err.Error(),
-			})
-		}
-		err, response := userHandler.GetUserName(c, model)
-		if err != nil {
-			return c.JSON(http.StatusBadRequest, map[string]interface{}{
-				"error": err.Error(),
-			})
-		}
-		return c.JSON(http.StatusOK, response)
-	})
-}
-
 var goTemplate = `
 package {{	.ProtoFileName}}
 
@@ -47,9 +11,6 @@ import (
 )
 {{-	end }}
 
-
-
-// messages
 {{-	range .Messages}}
 type {{.Name}} struct {
 	{{-	range .Fields}}
@@ -72,6 +33,27 @@ type {{.Name}} struct {
 				{{.Name}}(c echo.Context, request {{.Request}}) (err error, response {{.Response}})
 			{{-	end}}
 		}
+		func Init{{.Name}}HandlerRouter(e *echo.Echo, handler {{.Name}}) {
+			{{-	range .Methods}}
+			e.POST("{{.Url}}", func(c echo.Context) error {
+				var model {{.Request}}
+				err := c.Bind(&model)
+				if err != nil {
+					return c.JSON(http.StatusBadRequest, map[string]interface{}{
+						"error": err.Error(),
+					})
+				}
+				err, response := handler.{{.Name}}(c, model)
+				if err != nil {
+					return c.JSON(http.StatusBadRequest, map[string]interface{}{
+						"error": err.Error(),
+					})
+				}
+				return c.JSON(http.StatusOK, response)
+			})
+			{{-	end}}
+		}
 	{{-	end}}
 {{-	end }}
+
 `
